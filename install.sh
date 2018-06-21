f67afda0c4bd21833e9a506b60580631794b7e05
nimbus117 <mail@nimbus117.co.uk>
2018-06-21 16:26:35 +0100

    made this a bit safer with some cecks and a prompt before running

--- install.sh
+++ install.sh
@@ -3,12 +3,24 @@
 # chmod 744 install.sh
 # ./install.sh
 
+echo 'WARNING! - this script will replace configuration files on your system.'
+echo 'Read through the script before running it. Press [enter] to continue...'
+read
+
 # create symlinks from pwd to home directory
-declare -a arr=(".gitconfig" ".screenrc" ".zshrc" ".vimrc" ".config")
+declare -a arr=(".gitconfig" ".screenrc" ".zshrc" ".vimrc")
 for i in "${arr[@]}"
 do
-	echo $i
-	rm ~/$i -rf
-	ln -sf $(pwd)/$i ~/$i
+	echo linking $(pwd)/$i to $HOME/$i
+	ln -fs $(pwd)/$i $HOME/$i
 done
-echo "done"
+
+# create folders and symlinks from pwd\.config to home directory\.config
+folderPath=.config/git
+fileName=ignore
+if [ ! -d $HOME/$folderPath ]; then
+	echo creating $HOME/$folderPath
+	mkdir $HOME/$folderPath -p
+fi
+echo linking $(pwd)/$folderPath/$fileName to $HOME/$folderPath/$fileName
+ln -fs $(pwd)/$folderPath/$fileName $HOME/$folderPath/$fileName
