b15c6f356da0ed5a974f799557c5a6d63b7fccc8
nimbus117 <mail@nimbus117.co.uk>
2018-06-27 13:29:13 +0100

    added verbose option for ln

--- install.sh
+++ install.sh
@@ -7,10 +7,9 @@ echo 'WARNING! - this script will replace configuration files on your system.'
 echo 'Read through the script before running it. Press [enter] to continue...'
 read
 
-# create symlinks from pwd to home directory
+# create symlinks
 declare -a arr=(".gitconfig" ".screenrc" ".zshrc" ".vimrc" ".gitignore_global")
 for i in "${arr[@]}"
 do
-	echo linking $(pwd)/$i to $HOME/$i
-	ln -fs $(pwd)/$i $HOME/$i
+	ln -fsv $(pwd)/$i $HOME/$i
 done
