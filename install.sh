2c1bd0209e4854a3e8e4a739a06a1466f7149ae8
nimbus117 <mail@nimbus117.co.uk>
2018-10-23 19:59:28 +0100

    added custom zsh theme, changed folder structure and updated install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,15 @@ echo 'WARNING! - this script will replace configuration files on your system.'
 echo 'Read through the script before running it. Press [enter] to continue...'
 read
 
-# create symlinks
-declare -a arr=(".gitconfig" ".gitignore_global" ".screenrc" ".vimrc" ".zshrc")
+# create symlinks in home directory
+declare -a arr=(".gitconfig" ".gitignore_global" ".screenrc" ".vimrc" "zsh/.zshrc")
 for i in "${arr[@]}"
 do
-	ln -fsv $(pwd)/$i $HOME/$i
+  ln -fsv $(pwd)/$i $HOME/${i##*/}
 done
 
 # symlink for IntelliJ vimrc
 ln -fsv "$HOME/.vimrc" "$HOME/.ideavimrc"
+
+# symlink for mySimple zsh theme
+ln -fsv "$(pwd)/zsh/mySimple.zsh-theme" "$HOME/.oh-my-zsh/themes/mySimple.zsh-theme"
---
+++ zsh/mySimple.zsh-theme
@@ -0,0 +1,6 @@
+PROMPT='%{$fg_bold[blue]%}%m %(!.%{$fg[red]%}.%{$fg[green]%})%~%{$fg[blue]%}$(git_prompt_info)%{$reset_color%} '
+
+ZSH_THEME_GIT_PROMPT_PREFIX=" "
+ZSH_THEME_GIT_PROMPT_SUFFIX=""
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}✗"
+ZSH_THEME_GIT_PROMPT_CLEAN=" %{$fg[green]%}✔"
