a44270cb81cf908ac0b291b573164e514c4d71cd
nimbus117 <mail@nimbus117.co.uk>
2020-01-20 09:36:06 +0000

    update warning message

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 # chmod 744 install.sh
 # ./install.sh
 
-echo 'WARNING! - this script will replace configuration files on your system.'
-echo 'Read through the script before running it. Press [enter] to continue...'
+clear
+echo -e '\nWARNING! - This script may overwrite user configuration files.
+Please read it carefully before running it.
+Press [enter] to continue...'
 read
 
 # create links in home directory
