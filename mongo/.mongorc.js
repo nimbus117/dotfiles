1ff6191fe2696cc52b918c7e86380b9fe565e812
nimbus117 <mail@nimbus117.co.uk>
2020-08-07 12:16:52 +0100

    add ugly function to disable pretty shell output

--- mongo/.mongorc.js
+++ mongo/.mongorc.js
@@ -1,2 +1,8 @@
 // always pretty print results
-DBQuery.prototype._prettyShell = true
+DBQuery.prototype._prettyShell = true;
+
+// don't pretty print results
+DBQuery.prototype.ugly = function () {
+  this._prettyShell = false;
+  return this;
+};
