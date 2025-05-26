cfa5122739770faf22c063d0e9cbbc7a681b10bf
nimbus117 <mail@nimbus117.co.uk>
2025-05-26 11:10:18 +0100

    fix missing fields in mason lsp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -280,6 +280,8 @@ require("lazy").setup({
 				require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 				require("mason-lspconfig").setup({
+					ensure_installed = {},
+					automatic_enable = true,
 					handlers = {
 						function(server_name)
 							local server = servers[server_name] or {}
